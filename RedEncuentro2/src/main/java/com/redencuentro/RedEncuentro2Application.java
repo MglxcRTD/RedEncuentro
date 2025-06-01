5790e50b15a4537439546430181e9bfcbabc602e
Miguel <MiguelRoadToDam@gmail.com>
2025-06-01 18:10:14 +0200

    redencuentro funcional
    TODOS LOS HTML TIENEN QUE IR A LA CARPETA TEMPLATES QUE ESTA EN LA SIGUIENTE RUTA:
    C:\RedEncuentro2\src\main\resources\templates

---
+++ RedEncuentro2/src/main/java/com/redencuentro/RedEncuentro2Application.java
@@ -0,0 +1,13 @@
+package com.redencuentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedEncuentro2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedEncuentro2Application.class, args);
+	}
+
+}
---
+++ RedEncuentro2/src/main/java/com/redencuentro/repository/ObjetoPerdidoRepository.java
@@ -0,0 +1,9 @@
+package com.redencuentro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.redencuentro.entity.ObjetoPerdido;
+
+public interface ObjetoPerdidoRepository extends JpaRepository<ObjetoPerdido, Long> {
+}
+
